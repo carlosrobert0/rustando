cfd29ef7ead9667e1604c7e8e2fa9716b8012e50
carlosrobert0 <carlosroberto15755@gmail.com>
2026-03-28 11:12:24 -0300

    add gitignore

--- restaurant/target/debug/deps/restaurant-d3cd4502b5ea81ac.d
+++
@@ -1,7 +0,0 @@
-D:\rust\restaurant\target\debug\deps\restaurant-d3cd4502b5ea81ac.d: src\lib.rs src\front_of_house\mod.rs src\front_of_house\hosting.rs
-
-D:\rust\restaurant\target\debug\deps\librestaurant-d3cd4502b5ea81ac.rmeta: src\lib.rs src\front_of_house\mod.rs src\front_of_house\hosting.rs
-
-src\lib.rs:
-src\front_of_house\mod.rs:
-src\front_of_house\hosting.rs:
